0c0f1eab268acbfaed7c991075695be20eb811cd
Aziza  iliasova <miacorporation25@gmail.com>
2021-06-15 20:07:45 +0000

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,32 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const glyphs=document.getElementsByClassName('like-glyph');
+ const like=(e) =>{
+   const heart = e.target;
+   mimicServerCall("url")
+     .then(()=>{
+       if(heart.innerText===EMPTY_HEART){
+         heart.innerText=FULL_HEART
+         heart.classList.add('activated-heart')
 
+       }else{
+         heart.innerText=EMPTY_HEART
+         heart.classList.remove('activated-heart')
+       }
+     })
+     .catch(e =>{
+       const modal = document.getElementById("modal");
+       modal.className = "";
+       modal.innerText = e;
+       setTimeout(() =>  modal.className = "hidden", 2000);
+     });
+ }
 
+ [...glyphs].forEach((glyph)=>{
+   glyph.onclick=(e)=>like(e);
+
+ })
 
 
 //------------------------------------------------------------------------------
@@ -14,11 +38,11 @@ const FULL_HEART = '♥'
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
+      let fail = Math.random() < .2
+      if (fail) {
+        reject("Error!");
       } else {
-        resolve("Pretend remote server notified of action!");
+        resolve("New action");
       }
     }, 300);
   });
